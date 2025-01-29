5919668799865716fb4a1c637683ee506694c91a
Shoishob007 <u1704049@student.cuet.ac.bd>
2025-01-29 18:00:19 +0600

    Features cards complete for now

--- src/app/components/Hero Section/Hero.jsx
+++ src/app/components/Hero Section/Hero.jsx
@@ -4,7 +4,7 @@ import HeroRight from "./Hero-Right";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen flex items-center justify-center bg-white">
+    <section className="min-h-screen flex items-center justify-center" id="hero">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-6 md:px-16">
         <HeroLeft />
         <HeroRight />
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero Section/Hero";
+import Features from "./components/Key-Features Section/Features";
 
 const Homepage = () => {
   return (
     <>
       <Hero />
+      <Features />
     </>
   );
 };
