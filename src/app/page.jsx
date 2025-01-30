e7117af4bfed8d10cbfe3d79541d921447442909
emamjion <emamjion@gmail.com>
2025-01-30 08:36:29 +0600

    I added footer and why choose hirehub section initially

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero Section/Hero";
 import HowItWorks from "./components/How-It-Works/HowItWorks";
 import Features from "./components/Key-Features Section/Features";
+import WhyChooseHireHub from "./components/WhyChooseUs/WhyChosseHireHub";
 
 const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ const Homepage = () => {
       <Hero />
       <Features />
       <HowItWorks />
+      <WhyChooseHireHub />
     </>
   );
 };
