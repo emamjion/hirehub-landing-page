176ac1469aaf688be7b7029c3394ebc25f71f3a2
emamjion <emamjion@gmail.com>
2025-02-03 10:07:34 +0600

    added icons color and size

--- src/app/components/Key-Features Section/Features.jsx
+++ src/app/components/Key-Features Section/Features.jsx
@@ -1,10 +1,10 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Briefcase, Users, Calendar, BarChart } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { BarChart, Briefcase, Calendar, Users } from "lucide-react";
 
 const features = [
   {
     title: "Effortless Job Posting",
-    icon: <Briefcase className="w-8 sm:w-10 h-8 sm:h-10 text-primary" />,
+    icon: <Briefcase className="w-8 sm:w-10 h-8 sm:h-10 text-slate-500" />,
     details: [
       "Post unlimited jobs with the Pro plan.",
       "Easily create and publish job postings with just a few clicks.",
@@ -14,7 +14,7 @@ const features = [
   },
   {
     title: "Powerful Candidate Screening",
-    icon: <Users className="w-8 sm:w-10 h-8 sm:h-10 text-primary" />,
+    icon: <Users className="w-8 sm:w-10 h-8 sm:h-10 text-slate-500" />,
     details: [
       "Download applicant CVs with ease.",
       "Quickly review resumes.",
@@ -24,7 +24,7 @@ const features = [
   },
   {
     title: "Streamlined Interview Scheduling",
-    icon: <Calendar className="w-8 sm:w-10 h-8 sm:h-10 text-primary" />,
+    icon: <Calendar className="w-8 sm:w-10 h-8 sm:h-10 text-slate-500" />,
     details: [
       "Schedule and manage interviews with ease.",
       "Send automated interview reminders to candidates.",
@@ -33,7 +33,7 @@ const features = [
   },
   {
     title: "Data-Driven Insights",
-    icon: <BarChart className="w-8 sm:w-10 h-8 sm:h-10 text-primary" />,
+    icon: <BarChart className="w-8 sm:w-10 h-8 sm:h-10 text-slate-500" />,
     details: [
       "Advanced analytics and reporting features available.",
       "Track key recruitment metrics, such as time-to-hire and cost-per-hire.",
@@ -46,14 +46,21 @@ const Features = () => {
   return (
     <section className="py-8 sm:py-10" id="features">
       <div className="max-w-5xl mx-auto text-center mb-6 md:mb-10">
-        <h2 className="text-2xl sm:text-3xl font-bold text-primary">Key Features</h2>
+        <h2 className="text-2xl sm:text-3xl font-bold text-primary">
+          Key Features
+        </h2>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
         {features.map((feature, index) => (
-          <Card key={index} className="shadow-md border border-border rounded-xl bg-background">
+          <Card
+            key={index}
+            className="shadow-md border border-border rounded-xl bg-background"
+          >
             <CardHeader className="flex flex-row items-center gap-3 ">
               {feature.icon}
-              <CardTitle className="text-base font-semibold">{feature.title}</CardTitle>
+              <CardTitle className="text-base font-semibold">
+                {feature.title}
+              </CardTitle>
             </CardHeader>
             <CardContent>
               <ul className="list-disc pl-4 space-y-2 text-sm text-muted-foreground">
